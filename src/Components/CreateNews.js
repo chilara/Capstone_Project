2c1934bfce6af9ac8a16e6f3b1e02ca06b0cb295
Chisom Timothy Oguine <89150318+chilara@users.noreply.github.com>
2023-04-10 20:57:33 +0100

    fifth commit

--- src/Components/CreateNews.js
+++ src/Components/CreateNews.js
@@ -1,32 +1,151 @@
+import { Formik } from "formik";
+import * as yup from "yup";
 import React from "react";
-import Navbar from "./Navbar";
+import Navbar from "../Components/Navbar";
+import { base_url } from "../Utils/Constant";
+import axios from "axios";
 
 const CreateNews = () => {
+  const initialValues = {
+    author: "",
+    title: "",
+    url: "",
+    avatar: "",
+  };
+  const validationSchema = yup.object().shape({
+    author: yup.string().required("name is required"),
+    title: yup.string().required("title is required"),
+    url: yup.string().required("url is required"),
+    avatar: yup.string().required("avatar is required"),
+  });
   return (
     <div>
       <Navbar />
-      <p id="serverMsg"></p>
-      <form id="createForm">
-        <h3 class="fill">Fill Details</h3>
-        <br />
-        <label for="">Author</label>
-        <input type="text" id="author1" required />
-        <br />
-        <label for="">News Title</label>
-        <input type="text" id="newsTitle" required />
-        <br />
-        <label for="">News URL</label>
-        <input type="text" id="newsUrl" required />
-        <br />
-        <label for="">Avatar Link</label>
-        <input type="text" id="avatarLink" required />
-        <br />
-        <button type="submit" id="createBtn">
-          Create
-        </button>
-      </form>
+      <div>
+        <Formik
+          initialValues={initialValues}
+          validationSchema={validationSchema}
+          onSubmit={async (values, { setSubmitting }) => {
+            console.log(values);
+            let body = {
+              method: "post",
+              url: `${base_url}/news`,
+              headers: {
+                "Content-Type": "application/json",
+              },
+              data: values,
+            };
+
+            try {
+              const response = await axios(body);
+              console.log(JSON.stringify(response.data));
+            } catch (error) {
+              alert(error);
+            }
+          }}
+        >
+          {({
+            values,
+            errors,
+            touched,
+            handleChange,
+            handleBlur,
+            handleSubmit,
+            setFieldValue,
+            setFieldTouched,
+            isSubmitting,
+          }) => (
+            <form onSubmit={handleSubmit}>
+              <h3 class="fill">Fill Details</h3>
+              <br />
+              <div>
+                <label for="">Author</label>
+                <input
+                  id="author1"
+                  type="text"
+                  value={values.author}
+                  name="author"
+                  onChange={handleChange}
+                  onBlur={handleBlur}
+                />
+                <small
+                  style={{
+                    display: "block",
+                    color: "red",
+                    textAlign: "center",
+                  }}
+                >
+                  {touched.author && errors.author}
+                </small>
+              </div>
+              <div>
+                <label for="">News Title</label>
+                <input
+                  id="newsTitle"
+                  type="text"
+                  value={values.title}
+                  name="title"
+                  onChange={handleChange}
+                  onBlur={handleBlur}
+                />
+                <small
+                  style={{
+                    display: "block",
+                    color: "red",
+                    textAlign: "center",
+                  }}
+                >
+                  {touched.last_name && errors.last_name}
+                </small>
+              </div>
+              <div>
+                <label for="">News URL</label>
+                <input
+                  id="newsUrl"
+                  type="text"
+                  value={values.url}
+                  name="url"
+                  onChange={handleChange}
+                  onBlur={handleBlur}
+                />
+                <small
+                  style={{
+                    display: "block",
+                    color: "red",
+                    textAlign: "center",
+                  }}
+                >
+                  {touched.email && errors.email}
+                </small>
+              </div>
+              <div>
+                <label for="">Avatar Link</label>
+                <input
+                  id="avatarLink"
+                  type="text"
+                  value={values.avatar}
+                  name="avatar"
+                  onChange={handleChange}
+                  onBlur={handleBlur}
+                />
+                <small
+                  style={{
+                    display: "block",
+                    color: "red",
+                    textAlign: "center",
+                  }}
+                >
+                  {touched.password && errors.password}
+                </small>
+              </div>
+              <button type="submit" id="createBtn">
+                Create
+              </button>
+            </form>
+          )}
+        </Formik>
+      </div>
     </div>
   );
 };
-
 export default CreateNews;
--- src/Components/NewsDetails.js
+++ src/Components/NewsDetails.js
@@ -65,7 +65,6 @@ const NewsDetails = () => {
           />
         </div>
       )}
-      ;
     </div>
   );
 };
